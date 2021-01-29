8f47973b2326e85ec2eca1576e7b06c7a333c713
Jasper Eenhoorn <eenhoorn.jasper@gmail.com>
2021-01-29 17:44:06 +0100

    Vraag 1 update.

--- 4v-wia-h4-toets-01.py
+++ 4v-wia-h4-toets-01.py
@@ -1,7 +1,7 @@
-import numpy, datetime
+import numpy, datetime, scipy
 vraag = 2021012901
 seed = vraag+(datetime.date.today()-datetime.date(1,1,1)).days
-numpy.random.seed(seed)
+# numpy.random.seed(seed)
 
 def combinatie(n,k):
     return str(n)+"C"+str(k)
@@ -17,11 +17,13 @@ print(f"Getalwoorden\n Een: {getalwoorden[1]} \n")
 ida = random(4,7,1) # 4 t/m 7
 rij = getalwoorden[ida][1] # vierde t/m zevende
 combinaties = ", ".join([combinatie(ida-1,i) for i in range(0,ida)]) # vierde: 3C0, 3C1, 3C2, 3C#
-print(f"Opgave 1a\n Rij: {rij}\n Antwoord: {combinaties}\n")
+wcombinaties = ", ".join([str(int(scipy.special.comb(ida-1,i))) for i in range(0,ida)])
+print(f"Opgave 1a\n Rij: {rij}\n Antwoord: {combinaties} ({wcombinaties}).\n")
 
 # Opgave 1b
-idb = random(1,10,1) # een t/m 10, zero based
+idb = random(2,9,1) # twee t/m tien
 A = getalwoorden[idb][0] # vier
 B = getalwoorden[11-idb][0] # zeven
-aantal = combinatie(11,idb) + " en " + combinatie(11,11-idb)
-print(f"Opgave 1b\n Er zijn {A} A's en {B} B's.\n Aantal: {aantal}.")
+aantal = combinatie(11,idb) + " of " + combinatie(11,11-idb)
+waantal = str(int(scipy.special.comb(11,idb)))
+print(f"Opgave 1b\n Er zijn {A} A's en {B} B's.\n Aantal: {aantal} ({waantal}).\n")
